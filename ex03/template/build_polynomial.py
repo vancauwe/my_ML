8c3b1e00f828751de96d16b5424ea81aaf8fe35a
LaureV <laure.vancauwenberghe@epfl.ch>
2018-10-11 09:00:30 +0200

    ex3

--- ex03/template/build_polynomial.py
+++ ex03/template/build_polynomial.py
@@ -8,8 +8,12 @@ def build_poly(x, degree):
     """polynomial basis functions for input data x, for j=0 up to j=degree."""
     # ***************************************************
     # INSERT YOUR CODE HERE
-    # polynomial basis function: TODO
+    # polynomial basis function: 
+    theta=np.zeros((x.shape[0],degree))     
+    
+    for j in range(degree):
+        theta[:,j]=x**(j+1)
     # this function should return the matrix formed
     # by applying the polynomial basis to the input data
     # ***************************************************
-    raise NotImplementedError
+    return theta
